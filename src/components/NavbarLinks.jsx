f36c416a596d529f52261b426fc68c1c5b16c87a
Richard Gigi <gigirichardofficial@gmail.com>
2024-07-24 20:31:11 +0100

    Fixed navbar issue

---
+++ src/components/NavbarLinks.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavbarLinks = (props) => {
+    const { sidebar, active } = props;
+    return (
+        <div className={'main xui-d-flex ' + (sidebar ? ' sidebar xui-flex-dir-column xui-grid-gap-3 ' : ' xui-flex-ai-center xui-grid-gap-1-half ') + (active ? ' active ' : ' not-active ')}>
+            <Link to={'/'}>Programs</Link>
+            <Link to={'/'}>About</Link>
+        </div>
+    );
+};
+
+export default NavbarLinks;
